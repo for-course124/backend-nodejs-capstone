ec18cdf15e66c451ab2ff56ce94eefc810a69683
for-course124 <krzysiu0058@gmail.com>
2025-11-30 12:02:17 -0500

    login endppoint

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -42,3 +42,39 @@ router.post('/register', async (req, res) => {
          return res.status(500).send('Internal server error');
     }
 });
+
+router.post('/login', async (req, res) => {
+    try {
+        // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectToDatabase();
+        // Task 2: Access MongoDB `users` collection
+        const collection = db.collection("users");
+        // Task 3: Check for user credentials in database
+        const theUser = await collection.findOne({ email: req.body.email });
+        // Task 4: Check if the password matches the encrypted password and send appropriate message on mismatch
+        if (theUser) {
+            let result = await bcryptjs.compare(req.body.password, theUser.password)
+            if(!result) {
+                logger.error('Passwords do not match');
+                return res.status(404).json({ error: 'Wrong pasword' });
+            }
+            //continue other tasks
+             const userName = theUser.firstName;
+            const userEmail = theUser.email;
+        // Task 6: Create JWT authentication if passwords match with user._id as payload
+            let payload = {
+                user: {
+                    id: theUser._id.toString(),
+                },
+            };
+            jwt.sign(user._id, JWT_SECRET)
+            res.json({authtoken, userName, userEmail });
+            } else {
+            logger.error('User not found');
+            return res.status(404).json({ error: 'User not found' });
+        }
+    } catch (e) {
+         return res.status(500).send('Internal server error');
+
+    }
+});
